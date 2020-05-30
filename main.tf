d667e164af53ca830e65c2f280d902c8dde558be
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2020-05-30 03:00:24 -0700

    Move key_name from data block to resource block

--- main.tf
+++ main.tf
@@ -7,8 +7,7 @@ provider "aws" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  key_name   = var.key_name
-  
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
@@ -25,6 +24,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "netbox" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name      = var.key_name
 
   tags = {
     Name = "netbox-prod"
