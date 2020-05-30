7b5a92c71aa5322debd47311105aceb3590632ca
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2020-05-30 02:55:29 -0700

    Moving variable from provider block to data block

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.access_key
   secret_key = var.secret_key
-  key_name   = var.key_name
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-
+  key_name   = var.key_name
+  
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
