65412bdea1f01108d1453397d0a9149c9593e33c
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2020-05-30 01:40:26 -0700

    First Commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "netbox" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "netbox-prod"
+  }
+}
